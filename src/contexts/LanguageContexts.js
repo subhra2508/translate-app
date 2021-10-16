ab0810833175a8551945cd84ededb5984fbe7918
subhrajyoti <subhrajyotisankarsau@gmail.com>
2021-10-16 12:03:17 +0530

    using context object
    creating context object and use provider to udpate the value of the context object

--- src/contexts/LanguageContexts.js
+++ src/contexts/LanguageContexts.js
@@ -2,4 +2,9 @@ import React from 'react';
 
 
 
-export default React.createContext('english');
+const context = React.createContext('english');
+
+
+console.log(context);
+
+export default context;
